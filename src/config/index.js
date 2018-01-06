c337433bc889b372b2f81f02344cc39a6efd9130
cjh <1418503647@qq.com>
2018-01-07 00:30:25 +0800

    修改 API 接口

--- src/config/index.js
+++ src/config/index.js
@@ -2,16 +2,15 @@ let apiDomain
 let domain
 
 if (process.env.NODE_ENV === 'production') {
-    // apiDomain = 'http://120.78.177.9:1026'
-    // apiDomain = 'https://api.yunser.com'
     domain = {
         // api: 'https://api.yunser.com'
-        api: 'http://java.api.yunser.com'
+        api: 'https://api.yunser.com'
     }
 } else {
     domain = {
         // api = 'http://localhost:1026'
-        api: 'http://localhost:1030',
+        api: 'https://api.yunser.com',
+        // api: 'http://localhost:1030',
         img: 'img1.yunser.com'
     }
 }
